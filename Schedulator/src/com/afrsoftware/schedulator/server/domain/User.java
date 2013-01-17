feda62b195ca53fada01f2860d662796e04dc8c0
bgagnonadam <bgagnonadam@gmail.com>
2013-01-16 21:38:52 -0500

    + création des classes UserRepository et sont implémentation avec les JDO et commencement des tests.

--- Schedulator/src/com/afrsoftware/schedulator/server/domain/User.java
+++ Schedulator/src/com/afrsoftware/schedulator/server/domain/User.java
@@ -1,5 +1,80 @@
 package com.afrsoftware.schedulator.server.domain;
 
+import java.util.Date;
+
 public class User {
+
+	private String name;
+	private String password;
+	private String email;
+	private String adress;
+	private String city;
+	private String zipCode;
+	private String phone;
+	private Date birthDate;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date dateOfBirth) {
+		this.birthDate = dateOfBirth;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
 
 }
--- Schedulator/test/com/afrsoftware/schedulator/server/domain/UserTest.java
+++ Schedulator/test/com/afrsoftware/schedulator/server/domain/UserTest.java
@@ -1,5 +1,35 @@
 package com.afrsoftware.schedulator.server.domain;
 
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Test;
+
 public class UserTest {
+
+	@Test
+	public void canInstantiateAUser() {
+		User user = new User();
+
+		assertNotNull(user);
+	}
+
+	@Test
+	public void canSetDetailsToAUser() {
+		User user = new User();
+
+		user.setName("John Doe");
+		user.setAdress("1234 1st Avenue");
+		user.setCity("New Town");
+		user.setZipCode("G8ND5");
+		user.setBirthDate(new Date());
+		user.setEmail("myveryown@email.com");
+		user.setPassword("youshallnotpass");
+		user.setPhone("1-123-123-1234");
+
+		assertEquals("John Doe", user.getName());
+		assertEquals("1234 1st Avenue", user.getAdress());
+	}
 
 }
